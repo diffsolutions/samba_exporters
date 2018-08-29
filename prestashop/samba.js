391c8bac9821040cb660b185b8e337e653b9b7cf
mtd <mtd@prometheus.diff>
2018-08-29 15:40:44 +0200

    prestashop: order() is called when the "submit order" button is shown, not when is pressed (temporary solution)

--- prestashop/samba.js
+++ prestashop/samba.js
@@ -1,28 +1,37 @@
 <!-- Samba.ai scripts -->
 <script async src="https://yottlyscript.com/script.js?tp=720224"></script>
 <script type="text/javascript">
-function yt_run() {
-diffAnalytics.customerLoggedIn(prestashop.customer.email);
-sps = new Array();
-function to_sp(o) { return {productID: o.id_product, amount: o.cart_quantity}; };
-prestashop.cart.products.forEach(function(el) { sps.push(to_sp(el)) });
-diffAnalytics.cartInteraction({ content: sps });
-
-doc = document.querySelector("#payment-confirmation button");
-if (doc != null) {addEventListener("click", function(){
+function _yt_send_order(){
 sps = new Array();
 function to_sp(o) { return {productID: o.id_product, amount: o.quantity, price: o.quantity * o.price_with_reduction}; };
 prestashop.cart.products.forEach(function(el) { sps.push(to_sp(el)) });
 diffAnalytics.order({ content: sps });
-});
-}};
+};
+
+function yt_run() {
+if (prestashop.customer.email) {
+diffAnalytics.customerLoggedIn(prestashop.customer.email);
+};
+
+doc = document.querySelector("#payment-confirmation button");
+if (doc != null) {
+onOrderPage = true;
+//doc.addEventListener("click", _yt_send_order); 
+_yt_send_order();
+} else {
+onOrderPage = false;
+};
+
+sps = new Array();
+function to_sp(o) { return {productID: o.id_product, amount: parseInt(o.cart_quantity)}; };
+prestashop.cart.products.forEach(function(el) { sps.push(to_sp(el)) });
+diffAnalytics.cartInteraction({ content: sps, onOrderPage: onOrderPage });
+};
 
 var _yottlyOnload = _yottlyOnload || []
-_yottlyOnload.push(function () {
+_yottlyOnload.push(function () { 
 yt_run();
 });
 
 </script>
 <!-- End Samba.ai scripts -->
-
-
